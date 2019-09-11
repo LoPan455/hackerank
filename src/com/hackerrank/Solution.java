b11e5567837c40972131ce1518bc8b82a8db8f7b
Thomas Johander <tom.johander@dealerteamwork.com>
2019-09-11 07:53:29 -0500

    WIP on Beautiful Day

--- src/com/hackerrank/Solution.java
+++ src/com/hackerrank/Solution.java
@@ -1,9 +1,89 @@
 package com.hackerrank;
 
+import static java.lang.Integer.reverse;
+
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Lily likes to play games with integers. She has created a new game where she determines the
+ * difference between a number and its reverse. For instance, given the number , its reverse is .
+ * Their difference is . The number  reversed is , and their difference is .
+ *
+ * She decides to apply her game to decision making. She will look at a numbered range of days and
+ * will only go to a movie on a beautiful day.
+ *
+ * Given a range of numbered days,  and a number , determine the number of days in the range that
+ * are beautiful. Beautiful numbers are defined as numbers where  is evenly divisible by . If a
+ * day's value is a beautiful number, it is a beautiful day. Print the number of beautiful days in
+ * the range.
+ *
+ * Function Description
+ *
+ * Complete the beautifulDays function in the editor below. It must return the number of beautiful
+ * days in the range.
+ *
+ * beautifulDays has the following parameter(s):
+ *
+ * i: the starting day number j: the ending day number k: the divisor Input Format
+ *
+ * A single line of three space-separated integers describing the respective values of , , and .
+ *
+ * Constraints
+ *
+ * Output Format
+ *
+ * Print the number of beautiful days in the inclusive range between  and .
+ *
+ * Sample Input
+ *
+ * 20 23 6 Sample Output
+ *
+ * 2 Explanation
+ *
+ * Lily may go to the movies on days , , , and . We perform the following calculations to determine
+ * which days are beautiful:
+ *
+ * Day  is beautiful because the following evaluates to a whole number: Day  is not beautiful
+ * because the following doesn't evaluate to a whole number: Day  is beautiful because the following
+ * evaluates to a whole number: Day  is not beautiful because the following doesn't evaluate to a
+ * whole number: Only two days,  and , in this interval are beautiful. Thus, we print  as our
+ * answer.
+ */
 public class Solution {
 
-    public static void main(String[] args) {
+  // Complete the beautifulDays function below.
+  static int beautifulDays(int i, int j, int k) {
+    for (Integer l = i; l < j - i; l++) {
+      // Get an array of the digits of the current work item
+      char[] startArray = l.toString().toCharArray();
+
+      // Get the index of what will be the first char in the new array
+      int lastIndexOfArray = startArray.length - 1;
+
+      char[] reversedArray = startArray[lastIndexOfArray];
 
     }
-}
+  }
 
+
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    String[] ijk = scanner.nextLine().split(" ");
+
+    int i = Integer.parseInt(ijk[0]);
+
+    int j = Integer.parseInt(ijk[1]);
+
+    int k = Integer.parseInt(ijk[2]);
+
+    scanner.close();
+
+    int result = beautifulDays(i, j, k);
+
+    System.out.println(result);
+  }
+}
