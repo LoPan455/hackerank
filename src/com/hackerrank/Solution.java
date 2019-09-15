548c00bd89a462688e3cc7e48eda892411556be1
Thomas Johander <tom.johander@dealerteamwork.com>
2019-09-15 14:44:47 -0500

    Day 6 challenge solved

--- src/com/hackerrank/Solution.java
+++ src/com/hackerrank/Solution.java
@@ -1,8 +1,7 @@
 package com.hackerrank;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
@@ -10,9 +9,30 @@ public class Solution {
   private static final Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) throws IOException {
-    int n = scanner.nextInt();
+    int numberOfStrings = scanner.nextInt();
+    ArrayList<String> listOfStrings = new ArrayList<String>();
     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-    scanner.close();
+    while (numberOfStrings > 0) {
+      String string = scanner.nextLine();
+      listOfStrings.add(string);
+      numberOfStrings--;
+    }
+
+    for (String string : listOfStrings) {
+      char[] characterArray = string.toCharArray();
+      for (int i = 0; i < string.length(); i++) {
+        if (i % 2 == 0) {
+          System.out.print(characterArray[i]);
+        }
+      }
+      System.out.print(" ");
+      for (int i = 0; i < string.length(); i++) {
+        if (i % 2 != 0) {
+          System.out.print(characterArray[i]);
+        }
+      }
+      System.out.println();
+    }
   }
 }
 
