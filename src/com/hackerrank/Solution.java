313b0d546a19383ecac4fb2728d07db573e97d52
Thomas Johander <tom.johander@dealerteamwork.com>
2019-09-15 15:13:23 -0500

    Day 7 Challenge complete

--- src/com/hackerrank/Solution.java
+++ src/com/hackerrank/Solution.java
@@ -1,18 +1,33 @@
 package com.hackerrank;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
   private static final Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     int n = scanner.nextInt();
     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    int[] arr = new int[n];
+    String[] arrItems = scanner.nextLine().split(" ");
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    for (int i = 0; i < n; i++) {
+      int arrItem = Integer.parseInt(arrItems[i]);
+      arr[i] = arrItem;
+    }
+    for (int i = arr.length - 1; i >= 0 ; i--) {
+      System.out.print(arr[i]);
+      if (i != 0) {
+        System.out.print(" ");
+      }
+    }
     scanner.close();
   }
 }
-
