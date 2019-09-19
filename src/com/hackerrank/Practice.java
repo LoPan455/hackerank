f9edc23a55239a388cf21d11b43639ed8bed32d8
Thomas Johander <tom.johander@dealerteamwork.com>
2019-09-18 20:58:38 -0500

    2d Array Challenge solved

--- src/com/hackerrank/Practice.java
+++ src/com/hackerrank/Practice.java
@@ -1,46 +1,32 @@
 package com.hackerrank;
 
+import java.util.Arrays;
+
 public class Practice {
 
   public static int highestHourglassSum(int[][] array) {
 
-    int[] highestSum = new int[1];
+    int[] hourglassSums = new int[16];
+    int hourglassCounter = 0;
     for (int i = 0; i < array.length - 2; i++) {
       for (int j = 0; j < array[i].length - 2; j++) {
         int sumTopRow = array[i][j] + array[i][j + 1] + array[i][j + 2];
         int sumMiddleRow = array[i + 1][j + 1];
         int sumBottomRow = array[i + 2][j] + array[i + 2][j + 1] + array[i + 2][j + 2];
         int sumThisHourGlass = sumTopRow + sumMiddleRow + sumBottomRow;
-        if (sumThisHourGlass > highestSum[0]) {
-          highestSum[0] = sumThisHourGlass;
+        hourglassSums[hourglassCounter] = sumThisHourGlass;
+        hourglassCounter++;
         }
       }
+    Arrays.sort(hourglassSums);
+    return hourglassSums[15];
     }
-
-    // determine how to identify an hour glass
-
-    // for each hourglass:
-    // add the top row together
-    int sumTopRow = array[0][0] + array[0][1] + array[0][2];
-    int sumMiddleRow = array[1][1];
-    int sumBottomRow = array[2][0] + array[2][1] + array[2][2];
-    int sumFirstHourGlass = sumTopRow + sumMiddleRow + sumBottomRow;
-    // add the middle int
-    // add the bottom row
-    // store the value
-
-    // find the largeset value
-
-    return highestSum[0];
-  }
 
 
   public static void main(String[] args) {
-    int[][] array = {{1, 1, 1, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {0, 0, 2, 4, 4, 0},
-        {0, 0, 0, 2, 0, 0}, {0, 0, 1, 2, 4, 0}};
+    int[][] array = {{-1,-1,0,-9,-2,-2}, {-2,-1,-6,-8,-2,-5}, {-1,-1,-1,-2,-3,-4}, {-1,-9,-2,-4,-4,-5},
+        {-7,-3,-3,-2,-9,-9}, {-1,-3,-1,-2,-4,-5}};
     System.out.println(highestHourglassSum(array));
-
-
   }
 }
 
