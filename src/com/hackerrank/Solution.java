9ab27fb63fa4e5c0ed9975c49b5c6eab8891a32d
Thomas Johander <thomas.johnader@target.com>
2019-09-26 16:00:24 -0500

    Day 18 Challenge solved

--- src/com/hackerrank/Solution.java
+++ src/com/hackerrank/Solution.java
@@ -1,18 +1,58 @@
 package com.hackerrank;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
+  // Write your code here.
 
-  private static final Scanner scanner = new Scanner(System.in);
+  public LinkedList stack = new LinkedList();
+  public LinkedList queue = new LinkedList();
 
-  public static void main(String[] args) throws IOException {
-    int n = scanner.nextInt();
-    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-    scanner.close();
+  public void pushCharacter(char c) {
+    this.stack.addLast(c);
+  }
+
+  public void enqueueCharacter(char c) {
+    this.queue.add(c);
+  }
+
+  public char popCharacter() {
+    return (char) this.stack.removeLast();
+  }
+
+  public char dequeueCharacter() {
+    return (char) this.queue.remove(0);
+  }
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+    scan.close();
+
+    // Convert input String to an array of characters:
+    char[] s = input.toCharArray();
+
+    // Create a Solution object:
+    Solution p = new Solution();
+
+    // Enqueue/Push all chars to their respective data structures:
+    for (char c : s) {
+      p.pushCharacter(c);
+      p.enqueueCharacter(c);
+    }
+
+    // Pop/Dequeue the chars at the head of both data structures and compare them:
+    boolean isPalindrome = true;
+    for (int i = 0; i < s.length/2; i++) {
+      if (p.popCharacter() != p.dequeueCharacter()) {
+        isPalindrome = false;
+        break;
+      }
+    }
+
+    //Finally, print whether string s is palindrome or not.
+    System.out.println( "The word, " + input + ", is "
+            + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
   }
 }
-
